c92b645ee59602f52ac942d2cd38a6a85f506153
Kiiyo <andreitogni@gmail.com>
2022-09-22 11:23:55 +0800

    design

--- js/form.js
+++ js/form.js
@@ -1,14 +1,22 @@
-function addForm(){
+function addEntity(){
 	document.getElementById("createNewEntity").style.display = "block";
+}
+function updateEntity(){
+	document.getElementById("updateEntity").style.display = "block";
+}
+function deleteEntity(){
+	document.getElementById("deleteEntity").style.display = "block";
 }
 function closeForm() {
   document.getElementById("createNewEntity").style.display = "none";
   document.getElementById("updateEntity").style.display = "none";
-  document.getElementById("closeEntity").style.display = "none";
+  document.getElementById("deleteEntity").style.display = "none";
 }
 window.onclick = function (event) {
-  let modal = document.getElementById('createNewEntity');
-  if (event.target == modal) {
+  let modalx = document.getElementById('createNewEntity');
+  let modaly = document.getElementById('updateEntity');
+  let modalz = document.getElementById('deleteEntity');
+  if (event.target == modalx || event.target == modaly || event.target == modalz) {
     closeForm();
   }
 }
