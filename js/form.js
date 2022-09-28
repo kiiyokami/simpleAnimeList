a14c89bfae696d0d9a116c43161dcd220908f001
Kiiyo <andreitogni@gmail.com>
2022-09-28 12:47:44 +0800

    FINAL
    final changes

--- js/form.js
+++ js/form.js
@@ -2,24 +2,16 @@ function addEntity(){
 	closeForm();
 	document.getElementById("createNewEntity").style.display = "block";
 }
-function updateEntity(){
-	closeForm();
-	document.getElementById("updateEntity").style.display = "block";
-}
-function deleteEntity(){
-	closeForm();
-	document.getElementById("deleteEntity").style.display = "block";
-}
 function closeForm() {
   document.getElementById("createNewEntity").style.display = "none";
   document.getElementById("updateEntity").style.display = "none";
   document.getElementById("deleteEntity").style.display = "none";
 }
 window.onclick = function (event) {
-  let modalx = document.getElementById('createNewEntity');
-  let modaly = document.getElementById('updateEntity');
-  let modalz = document.getElementById('deleteEntity');
-  if (event.target == modalx || event.target == modaly || event.target == modalz) {
+  let x = document.getElementById('close');
+  let y = document.getElementById('shut');
+  let z = document.getElementById('down');
+  if (event.target == x|event.target == y|event.target == z) {
     closeForm();
   }
 }
